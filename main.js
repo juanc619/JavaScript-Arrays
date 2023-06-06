29365f2f1e9a768d81df4613786e168a9f6b7b4d
Juan Carlos <juancarlosvelasquez619@gmail.com>
2023-06-06 18:05:45 -0500

    recorriendo nuestro array

---
+++ main.js
@@ -0,0 +1,22 @@
+// Declarando nuestros primeros array 
+
+// const numeros = [1, 2, 3, 4, 5];
+// const booleanos = [true, true, false];
+// const mezcla = ["sillon", 1, 2, true];
+// const usuarios = [ {nombre: "jc", edad : 28},{nombre : "carlos",edad : 20}];
+
+
+
+
+const productos = ["sillon", "silla", "mesa", "ropero"];
+
+// Accedienco a nuestro Array
+
+// console.log(productos);
+// console.log(productos[2]);
+
+//  Recorriendo nuestro Array
+
+for(let i = 0; i < 4; i++) {
+    console.log(productos[i]);
+}
